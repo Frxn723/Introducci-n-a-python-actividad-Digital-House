0263d137f2f0e9547585a4a43f00b8a6e06c267b
Frxn723 <126273559+Frxn723@users.noreply.github.com>
2024-10-20 13:19:04 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+print("Actividad Entregable introducción a python - Historia Loca número 1")
+# La historia se sitúa en el pueblo ficticio de Hawkins, en el estado de Indiana, Estados Unidos, durante los años ochenta, 
+# cuando un niño de doce años llamado Will Byers desaparece misteriosamente. 
+# Poco después, Eleven, una niña aparentemente fugitiva y con poderes telequinéticos, 
+# se encuentra con Mike, Dustin y Lucas, amigos de Will, y los ayuda en la búsqueda de su amigo desaparecido.
+
+lugar = input("ingresa el nombre de la ciudad o el pueblo")
+pais = input("ingresa el nombre del país")
+nombre = input("ingresa el nombre del chico")
+nombre2 = input("ingresa el nombre de la chica")
+edad = input("ingresa la edad del chico")
+
+print("La historia se sitúa en el pueblo ficticio de" + lugar + ", en el estado de Indiana," + pais + " , durante los años ochenta," + " cuando un niño de" + edad + " años" +" llamado" + nombre + " desaparece misteriosamente.")
+
+print("Poco después," + nombre2 + " , una niña aparentemente fugitiva y con poderes telequinéticos, " + "se encuentra con Mike, Dustin y Lucas, amigos de Will, y los ayuda en la búsqueda de su amigo desaparecido.")
